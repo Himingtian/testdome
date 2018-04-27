270ddf24887a6662344d7028cef6cef630d233fe
MyGit <15018714832@163.com>
2018-04-27 23:23:37 +0800

    frist

---
+++ src/main/java/com/learnspring/demo01/TestDemo.java
@@ -0,0 +1,36 @@
+package com.learnspring.demo01;
+
+import org.junit.Test;
+
+public class TestDemo {
+
+
+    @Test
+    public void demo() {
+
+        int arr[] = {1, 5, 3, 8};
+        System.out.println(arr.length);
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            for (int j = 0; j < arr.length - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                   /* int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;*/
+
+                   arr[j+1] = arr[j] + arr[j+1];
+                   arr[j] = arr[j+1] - arr[j];
+                   arr[j+1] = arr[j+1] - arr[j];
+                }
+            }
+        }
+
+        for (int i : arr)
+        {
+            System.out.println(i);
+        }
+    }
+
+}
